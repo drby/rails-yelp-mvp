e11d3d7a77a9d837b954f367ab230667c19c2376
Tom Darby <tomd.57@live.com>
2019-11-13 16:31:07 +0100

    routes and controllers ready

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,10 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+
+5.times do
+  restaurants = Restaurant.create(name: Faker::Name.first_name, address: Faker::Address.street_address, category: %w[chinese italian japanese french belgian].sample)
+  ratings = Review.create(content: Faker::Lorem.sentence(word_count: 10), rating: [0, 1, 2, 3, 4, 5].sample, restaurant: restaurants)
+end
+
 #   Character.create(name: 'Luke', movie: movies.first)
